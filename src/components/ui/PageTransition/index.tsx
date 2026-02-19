163b7207b5b87f84609db922c62eec643d44dfe9
Lucas Silva <lucas.silvaaaa30@gmail.com>
2026-02-19 13:29:25 -0300

    feat: add page transitions

--- src/components/ui/PageTransition/index.tsx
+++ src/components/ui/PageTransition/index.tsx
@@ -1,27 +1,27 @@
 'use client';
 
-import { AnimatePresence, motion } from "motion/react";
+import { motion } from "motion/react";
 import { usePathname } from "next/navigation";
-import { useEffect } from "react";
+
+const variants = {
+    initial: { x: 16, opacity: 0 },
+    animate: { x: 0, opacity: 1 },
+    exit: { x: -16, opacity: 0 },
+}
 
 export default function PageTransition({ children }: { children: React.ReactNode }) {
     const pathname = usePathname();
 
-    useEffect(() => {
-        window.scrollTo(0, 0)
-    }, [pathname])
-
     return (
-        <AnimatePresence initial={true} mode="wait">
-            <motion.div
-                key={pathname}
-                initial={{ x: 16, opacity: 0 }}
-                animate={{ x: 0, opacity: 1 }}
-                exit={{ x: -16, opacity: 0 }}
-                transition={{ duration: 0.25, ease: 'easeInOut' }}
-            >
-                {children}
-            </motion.div>
-        </AnimatePresence>
+        <motion.div
+            key={pathname}
+            variants={variants}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{ type: 'tween', duration: 0.25, }}
+        >
+            {children}
+        </motion.div>
     );
 }
